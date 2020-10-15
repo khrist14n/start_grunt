be0a745f91422e1b8c28b2836246982ff8310c2e
vagrant <vagrant@vagrant.vm>
2020-10-15 12:52:25 +0000

    app

---
+++ 01_project/Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+    console.log("Inicio ejecucion grunt");
+    grunt.initConfig(
+        {
+
+        }
+    );
+    grunt.registerTask(
+        "default",
+        [
+
+        ]
+    );
+    console.log("Fin ejecucion grunt");
+}
